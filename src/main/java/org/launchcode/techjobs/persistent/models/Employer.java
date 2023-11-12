3a1462e61cc6c8b74b89eb050eee05bedaa909ff
Taylor016 <taylorr.016@gmail.com>
2023-11-11 21:21:05 -0600

    updates

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,33 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(max=100)
+    private String location;
+
+    @OneToMany(mappedBy = "employer")
+    @JoinColumn()
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
